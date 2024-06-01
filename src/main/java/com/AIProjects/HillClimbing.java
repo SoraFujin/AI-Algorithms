cce146f45eab446b18225b4c6d65450f496983ae
ahmad diab <ahmaddiab29072002@gmail.com>
2024-06-01 16:58:28 +0300

    add State and HillClimbing algo

--- src/main/java/com/AIProjects/HillClimbing.java
+++ src/main/java/com/AIProjects/HillClimbing.java
@@ -1,19 +1,46 @@
 package com.AIProjects;
 
-public class HillClimbing {
-	double[][] hTable = {{0, 0.68, 0.55, 0.30, 0.82, 0.48, 0.33, 0.10, 0.76, 0.43},
-			{0.68, 0, 0.90, 0.11, 0.76, 0.20, 0.55, 0.17, 0.62, 0.99},
-			{0.55, 0.90, 0, 0.70, 0.63, 0.96, 0.51, 0.90, 0.88, 0.64},
-			{0.30, 0.11, 0.70, 0, 0.91, 0.86, 0.78, 0.99, 0.53, 0.92},
-			{0.82, 0.76, 0.63, 0.91, 0, 0.43, 0.88, 0.53, 0.42, 0.75},
-			{0.48, 0.20, 0.96, 0.86, 0.43, 0, 0.63, 0.97, 0.37, 0.26},
-			{0.33, 0.55, 0.51, 0.78, 0.88, 0.63, 0, 0.92, 0.87, 0.81},
-			{0.10, 0.17, 0.90, 0.99, 0.53, 0.97, 0.92, 0, 0.81, 0.78},
-			{0.76, 0.62, 0.88, 0.53, 0.42, 0.37, 0.87, 0.81, 0, 0.45},
-			{0.43, 0.99, 0.64, 0.92, 0.75, 0.26, 0.81, 0.78, 0.45, 0}};
+import java.util.ArrayList;
+import java.util.List;
 
-	public HillClimbing(double[][] data) {
-		this.hTable = data;
+public class HillClimbing {
+	private double[][] data;
+	private List<Integer> arrangement;
+
+	public HillClimbing(double[][] data, List<Integer> arrangement) {
+		this.data = data;
+		this.arrangement = new ArrayList<>(arrangement);
 	}
 
+
+	public double calculate_cost(List<Integer> arrangement) {
+		double totalDislike = 0;
+		for (int i = 0; i < arrangement.size(); i++) {
+			int person1 = arrangement.get(i);
+			int person2 = arrangement.get((i + 1) % arrangement.size());
+			totalDislike += data[person1][person2];
+		}
+		return totalDislike;
+	}
+
+	public List<State> getNeighbor() {
+		List<State> neighbors = new ArrayList<>();
+		for (int i = 0; i < arrangement.size(); i++) {
+			for (int j = i + 1; j < arrangement.size(); j++) {
+				List<Integer> newArrangement = new ArrayList<>(arrangement);
+				swap(newArrangement, i, j);
+			}
+		}
+		return neighbors;
+	}
+
+	public void swap(List<Integer> list, int i, int j) {
+		int temp = list.get(i);
+		list.set(i, list.get(j));
+		list.set(j, temp);
+	}
+
+	public String toString() {
+		return "Seating arrangment " + arrangement + "\n dislike " + calculate_cost(arrangement);
+	}
 }
