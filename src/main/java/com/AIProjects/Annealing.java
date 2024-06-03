1ccc368cbfa2bafff5e2bfb678a0ff4ac1c1fa83
ahmad diab <ahmaddiab29072002@gmail.com>
2024-06-03 22:54:12 +0300

    Fix Simulated Annealing

--- src/main/java/com/AIProjects/Annealing.java
+++ src/main/java/com/AIProjects/Annealing.java
@@ -8,12 +8,13 @@ import java.util.List;
 import java.util.Random;
 
 public class Annealing {
-	private static List<Integer> arrangement = new ArrayList<>();
+	private List<Integer> arrangement = new ArrayList<>();
 
 	public Annealing(List<Integer> arrangment) {
 		this.arrangement = arrangment;
 	}
 
+	// Boltzmann Probability distribution
 	public static boolean acceptance(double temp, double deltaE) {
 		Random random = new Random();
 		if (deltaE < 0) {
@@ -24,23 +25,6 @@ public class Annealing {
 			} else
 				return false;
 		}
-	}
-
-	public List<List<Integer>> getNeighbor() {
-		List<List<Integer>> neighbors = new ArrayList<>();
-		for (int i = 0; i < arrangement.size(); i++) {
-			for (int j = i + 1; j < arrangement.size(); j++) {
-				List<Integer> newArrangement = new ArrayList<>(arrangement);
-				swap(newArrangement, i, j);
-			}
-		}
-		return neighbors;
-	}
-
-	public void swap(List<Integer> list, int i, int j) {
-		int temp = list.get(i);
-		list.set(i, list.get(j));
-		list.set(j, temp);
 	}
 }
 
